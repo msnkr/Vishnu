d426c5078ccaf3bd8cb7b1b9727933d1da319346
msnkr <mikyle.designs@gmail.com>
2022-06-17 12:27:22 +0200

    .

--- vish_project/vish_app/urls.py
+++ vish_project/vish_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import HomePageView, ProductListView, ProductDetailView, ContactView
 
 urlpatterns = [
@@ -7,3 +9,6 @@ urlpatterns = [
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail-view'),
     path('contact-us/', ContactView.as_view(), name='contact-us'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
